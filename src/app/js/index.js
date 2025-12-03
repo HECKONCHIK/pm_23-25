1a7c2cc7490f7c96788149f5380e68bd17cd9264
Oleh Tsap <olehtsap07@gmail.com>
2025-12-03 12:41:07 +0200

    Remove experience items from experience list

--- src/app/js/index.js
+++ src/app/js/index.js
@@ -1,6 +1,7 @@
 const firstName = document.getElementById("first-name");
 const lastName = document.getElementById("last-name");
 const arrows = document.querySelectorAll(".arrow");
+const expList = document.querySelector(".experience-list");
 
 const name = 'GARY T.';
 const surname = 'WALTON'
@@ -16,16 +17,74 @@ arrows.forEach(function(arrow) {
 
     if (!contents) return;
 
-    // Якщо приховано -> показуємо і крутимо стрілку
     if (contents.style.visibility === "hidden") {
       contents.style.visibility = "visible";
+      contents.style.display = "block";
       this.classList.add("active");
     }
-    // У всіх інших випадках (в т.ч. перший клік, якщо було visible) -> ховаємо і вертаємо стрілку
     else {
       contents.style.visibility = "hidden";
+      contents.style.display = "none";
       this.classList.remove("active");
     }
   });
 });
 
+
+const work = [
+  {
+    comp: 'Creative Agency',
+    loc: 'Chicago',
+    pos: 'SENIOR WEB DESIGNER',
+    date: '2021 - Present',
+    desc: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
+  },
+  {
+    comp: 'Creative Market',
+    loc: 'United Kingdom',
+    pos: 'GRAPHIC DESIGNER',
+    date: '2018 - 2021',
+    desc: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
+  },
+  {
+    comp: 'Marketing Agency',
+    loc: 'United Kingdom',
+    pos: 'MARKETING MANAGER',
+    date: '2015 - 2018',
+    desc: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
+  },
+  {
+    comp: 'Creative Market',
+    loc: 'Chicago',
+    pos: 'JUNIOR WEB DESIGNER',
+    date: '2013 - 2015',
+    desc: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
+  }
+];
+
+let listHTML = '';
+
+work.forEach(function(job) {
+  listHTML += `
+    <li class="experience-item">
+      <span class="decorator"></span>
+      <div class="content">
+        <div class="header-line">
+          <div class="title-location fst-italic">
+            <h3 class="company">${job.comp}</h3>
+            <p class="location">${job.loc}</p>
+          </div>
+          <span class="date">${job.date}</span>
+        </div>
+        <h4 class="position">${job.pos}</h4>
+        <p class="description">
+          ${job.desc}
+        </p>
+      </div>
+   </li>
+  `;
+})
+
+// console.log(listHTML);
+
+expList.innerHTML = listHTML;
